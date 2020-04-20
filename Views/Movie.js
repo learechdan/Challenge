ea40efcba09769d11e319885dd4f797fd307019b
Lea Rechdan <lea.rechdan@hotmail.com>
2020-04-20 17:29:29 +0300

    Update Movie.js

--- Views/Movie.js
+++ Views/Movie.js
@@ -2,7 +2,9 @@ const Movie = require('../Models/Movie');
 // var UserMovie_Service = require('../Services/UserMovie');
 
 getMovie = async function (req, res){ 
-    var movies = await Movie.find({});
+    // this will be applied for 1 or many movies
+    var query = req.params.movieTitle ? {title: req.params.movieTitle.toLowerCase()} : {};
+    var movies = await Movie.find(query); // mongodb search is case sensitive, so I have to search further in mongodb documentation about it, or we make sure that the titles are in lowercase 
     return movies;
 }
 
